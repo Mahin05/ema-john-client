3a52b7fe2c49a5c571b0c6d0a20f97f1e04de6ff
Mahin05 <mahin.islam05@gmail.com>
2022-05-22 22:26:01 +0600

    navbar set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-
+    <div  className='max-w-7xl	mx-auto px-12'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        </Routes>
     </div>
   );
 }
