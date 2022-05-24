84ba9a66ff3f2ce0cb3b2bffb3246792b74c96bd
Mahin05 <mahin.islam05@gmail.com>
2022-05-25 01:32:40 +0600

    user info add

--- src/Pages/Dashboard/Profile.js
+++ src/Pages/Dashboard/Profile.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import { toast, ToastContainer } from 'react-toastify';
+import auth from '../../firebase.init';
 
 const Profile = () => {
+    const [user] = useAuthState(auth);
+    const email = user.email;
     const { register, handleSubmit } = useForm();
     const onSubmit = data => {
         console.log(data);
-        const url = `http://localhost:5000/review`;
+        const url = `http://localhost:5000/userInfo/${email}`;
         fetch(url, {
             method: 'POST',
             headers: {
@@ -16,19 +20,27 @@ const Profile = () => {
         })
             .then(res => res.json())
             .then(result => {
-                toast('Thanks for your review!');
+                toast('Informations Added!');
                 console.log(result);
             })
     }
     return (
         <div>
             <h2 className="text-2xl">Profile</h2>
-            <form onSubmit={handleSubmit(onSubmit)}  className='purchase-form'>
-            <label>Review</label>
-                <input {...register("review")} name="review" type="number" class="feedback-input" placeholder="Your Review" />
+            <form onSubmit={handleSubmit(onSubmit)} className='purchase-form'>
+                <label>Name</label>
+                <input name="name" type="text" class="feedback-input" value={user.displayName} placeholder="Your Name" readOnly />
+                <label>Email</label>
+                <input name="email" type="email" class="feedback-input" value={user.email} placeholder="Email" readOnly />
+                <label>Education</label>
+                <input {...register("education")} name="education" type="text" class="feedback-input" placeholder="Your Education" />
                 <label>Description</label>
-                <textarea {...register("description")} className='purchase-textArea'  rows={2} name="description" class="feedback-input" placeholder="Your Feedback"></textarea>
-                <input className='purchase-submit' type="submit" value="Review" />
+                <textarea {...register("address")} className='purchase-textArea' rows={2} name="address" class="feedback-input" placeholder="Your Address"></textarea>
+                <label>Phone</label>
+                <input {...register("phone")} name="phone" type="number" class="feedback-input" placeholder="Your Phone Number" />
+                <label>Linked-In</label>
+                <input {...register("linkedIn")} name="linkedIn" type="text" class="feedback-input" placeholder="Your Linked-In Profile Link" />
+                <input className='purchase-submit' type="submit" value="Submit" />
             </form>
             <ToastContainer></ToastContainer>
         </div>
