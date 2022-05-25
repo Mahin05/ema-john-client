73bedadd1ddb1c1ab7db6b8ce957a45f9ed7742e
Mahin05 <mahin.islam05@gmail.com>
2022-05-26 00:44:23 +0600

    order function done

--- src/Pages/Home/PurchasePage/PurchasePage.js
+++ src/Pages/Home/PurchasePage/PurchasePage.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { FloatingLabel, Form, NavLink } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import auth from '../../../firebase.init';
+import Button from './Button';
 import './PurchasePage.css'
 
 const PurchasePage = () => {
     const [user] = useAuthState(auth);
+    const { register, handleSubmit } = useForm();
     const { toolId } = useParams();
-    // const [inventory] = useInventoryDetail(inventoryId);
-    // const { register, handleSubmit } = useForm();
     const [tool, setTool] = useState({});
-    const minimunOrderQuantity = parseInt(tool.minimunOrderQuantity);
+    // console.log(tool.name);
     // console.log(tool.minimunOrderQuantity);
     useEffect(() => {
         const url = `http://localhost:5000/purchase/${toolId}`;
@@ -21,21 +22,76 @@ const PurchasePage = () => {
             .then(data => setTool(data))
     }, [toolId])
 
+    // const handleAddressChange = event => {
+    //     console.log(event.target.value)
+    //     const { address, ...rest } = user;
+    //     const newAddress = event.target.value;
+    //     const newUser = { address: newAddress, ...rest };
+    //     // console.log(address,rest);
+    //     console.log(newUser);
+    //     setUser(newUser);
+    // }
+    const [error, setError] = useState('');
     const handleMinimumQuantity = event => {
-        console.log(minimunOrderQuantity);
-        // const {miminimunOrderQuantity, ...rest} = tool;
-        const orderQuantity = event.target.value;
-        console.log(orderQuantity);
-        if(orderQuantity < minimunOrderQuantity){
-            const error = `Please Minimu order`
-            toast(`You Must order ${minimunOrderQuantity} pieces`)
+        console.log(event.target.value);
+        const minimumQuantity = event.target.value;
+        const databaseMinimumQuantity = tool.minimunOrderQuantity;
+        const databaseAvailableQuantity = tool.availableQuantity;
+        if (minimumQuantity < databaseMinimumQuantity || minimumQuantity === '') {
+            // toast.error('Please select more then minimum order');
+            setError('Please place your orders between available & minimum order quantity!', true);
         }
-        // const newTool = {miminimunOrderQuantity:orderQuantity,...rest}
-        // console.log(newTool);
-        // setTool(newTool);
+        else if (minimumQuantity > databaseAvailableQuantity) {
+            setError('Please place your orders between available & minimum order quantity!', true);
+        }
+        else {
+            setError('');
+        }
     }
 
-    
+    const handleOrder = event => {
+        event.preventDefault();
+        const order = {
+            userName:user.displayName,
+            email:user.email,
+            phone:event.target.phone.value,
+            address:event.target.address.value,
+            toolName:event.target.toolName.value,
+            orderQuantity:event.target.orderQuantity.value
+        }
+        fetch('http://localhost:5000/order',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(order)
+        })
+        .then(res => res.json())
+        .then(result => {
+            toast('Item added successfully!!')
+            console.log(result);
+        })
+    }
+
+    const onSubmit = (data,event) => {
+        const toolName = event.target.toolName.value;
+        console.log(data);
+        const url = `http://localhost:5000/order`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data,toolName)
+        })
+            .then(res => res.json())
+            .then(result => {
+                toast('Item added successfully!!')
+                console.log(result);
+            })
+    }
+
+
     // const handleDelivered = event => {
     //     event.preventDefault()
     //     console.log(event.target.value)
@@ -97,55 +153,30 @@ const PurchasePage = () => {
 
     return (
         <div>
-            <form className='purchase-form'>
+            <form  onSubmit={handleOrder} className='purchase-form'>
                 <label>Name</label>
-                <input name="name" type="text" class="feedback-input" value={user.displayName} placeholder="Your Name" readOnly />
+                <input name="displayName" {...register("displayName")} type="text" class="feedback-input" value={user.displayName} placeholder="Your Name" readOnly />
                 <label>Email</label>
-                <input name="email" type="email" class="feedback-input" value={user.email}  placeholder="Email" readOnly />
+                <input name="email" {...register("email")} type="email" class="feedback-input" value={user.email} placeholder="Email" readOnly />
                 <label>Phone</label>
-                <input name="phone" type="number" class="feedback-input" placeholder="Your Phone" />
+                <input name="phone" {...register("phone")}  type="number" class="feedback-input" placeholder="Your Phone" required />
                 <label>Address</label>
-                <textarea className='purchase-textArea'  rows={2} name="address" class="feedback-input" placeholder="Your Address"></textarea>
+                <textarea className='purchase-textArea' rows={2} name="address" {...register("address")}  class="feedback-input" placeholder="Your Address"></textarea>
                 <label>Tool</label>
-                <input name="toolName" type="text" class="feedback-input" value={tool.name}  placeholder="Your Phone" readOnly/>
-                <label>Details</label>
-                <textarea className='purchase-textArea'   rows={2} name="toolDescription" class="feedback-input" value={tool.description}  placeholder="Your Address" readOnly></textarea>
+                <input name="toolName" {...register("toolName")} type="text" class="feedback-input" value={tool.name} placeholder={tool.name} readOnly />
                 <label>Price(per unit)</label>
-                <input name="pricePerunit" type="text" class="feedback-input" value={tool.pricePerUnit}  placeholder="Your Phone" readOnly/>
+                <input name="pricePerunit" type="text" class="feedback-input" value={tool.pricePerUnit} placeholder="Your Phone" readOnly />
                 <label>Available quantity</label>
-                <input name="availableQuantity" type="text" class="feedback-input" value={tool.availableQuantity} placeholder="Your Phone" readOnly/>
-                <label>Order quantity</label>
-                <input name="orderQuantity" onChange={handleMinimumQuantity} type="number" class="feedback-input" placeholder={tool.minimunOrderQuantity} />
-                <input className='purchase-submit' type="submit" value="SUBMIT" />
+                <input name="availableQuantity" type="text" class="feedback-input" value={tool.availableQuantity} placeholder="Your Phone" readOnly />
+                <label>Your order quantity</label>
+                <input name="orderQuantity" {...register("orderQuantity")} onChange={handleMinimumQuantity} type="number" class="feedback-input" placeholder={tool.minimunOrderQuantity} />
+                {error ? <input disabled className='purchase-submit' type="submit" value="Place Order" /> :
+                    <input className='purchase-submit' type="submit" value="Place Order" />}
+                <div className='text-red-500 text-3xl my-5'>
+                {error}
+                </div>
             </form>
-            {/* <form>
-                <img className='in-img' src={tool.img} alt="" />
-                <input className='w-100 mb-2' type="email" value={user?.email} name="email" placeholder='email' required readOnly disabled /> <br />
-                <input className='w-100 mb-2' type="text" value={toolId} name="inventoryId" placeholder='inventoryId' required readOnly disabled /> <br />
-                <input className='w-100 mb-2' type="text" value={tool.name} name="inventory" placeholder='service' required readOnly disabled /> <br />
-                <FloatingLabel controlId="floatingTextarea2" label={tool.description}>
-                    <Form.Control
-                        as="textarea"
-                        className='mb-2'
-                        placeholder="Leave a comment here"
-                        style={{ height: '100px' }}
-                        readOnly
-                        disabled
-                    />
-                </FloatingLabel>
-                <input className='w-100 mb-2' type="text" value={tool.price} name="price" placeholder='price' required readOnly disabled /> <br />
-                <input className='w-100 mb-2' type="text" value={tool.supplierName} name="price" placeholder='price' required readOnly disabled /> <br />
-                <input className='w-100 mb-2' value={tool.quantity} type="number" name="quantity" placeholder='quantity' required readOnly /> <br />
-                <input className='w-100 mb-2' type="number" name="quantity" placeholder={tool.quantity ? 'Stock Available' : 'Sold'} required readOnly /> <br /> <br />
-                <input className='btn btn-delivered' type="submit" value="Delivered" />
-            </form> */}
-            {/* <form onSubmit={handleRestock}>
-                <h2 className='restock'>Restock the item</h2>
-                <input className='w-100 mb-2' type="text" name="quantity" placeholder='quantity' required /> <br />
-                <input className='btnn' type="submit" value="Restock" />
-            </form> */}
 
-            {/* <NavLink as={Link} to="/manageInventory" className='mt-3 '><button className='in-btn-style' >Manage Inventories</button></NavLink> */}
         </div>
     );
 };
