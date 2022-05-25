86c360c6a1c1ea932ba5c9cf4ec6dadcbcd1f810
Mahin05 <mahin.islam05@gmail.com>
2022-05-26 01:21:34 +0600

    my order show

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -30,7 +30,7 @@ const Dashboard = () => {
                         <li><Link to="/dashboard/manageOrders">Manage All Orders</Link></li>
                         <li><Link to="/dashboard/addProduct">Add a Product</Link></li>
                         <li><Link to="/dashboard/users">Make Admin</Link></li>
-                        <li><Link to="/dashboard/ManageProducts">Manage Products</Link></li>
+                        <li><Link to="/dashboard/manage">Manage Products</Link></li>
                     </>}
                 </ul>
 
--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import axios from 'axios';
+
 
 const MyOrders = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/booking?email=${user.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data))
+        }
+    }, [user])
     return (
         <div>
-            <h2 className="text-2xl">My Orders</h2>
+            <h2 className="text-2xl">My Orders:{orders.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Tool Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map((order,index)=> 
+                            <tr>
+                                <th>{index + 1}</th>
+                                <td>{order.displayName}</td>
+                                <td>{order.email}</td>
+                                <td>{order.toolName}</td>
+                            </tr>)
+                        }
+                       
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
